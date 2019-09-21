d366daebbbb65031ca4d74aafc77346f77647f59
Samuel Alves <samuel241296@hotmail.com>
2019-09-21 11:03:44 -0300

    Criação das configuraçoes do spring, home.jsp e seu controller

---
+++ src/main/java/br/com/samuel/ecommerce/conf/ServletSpringMvc.java
@@ -0,0 +1,22 @@
+package br.com.samuel.ecommerce.conf;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServletSpringMvc extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+    	 return new Class[] { AppWebConfiguration.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+
+}
